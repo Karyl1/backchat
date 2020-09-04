b11491118ea1312b8a912c4a273dc4014c1053f7
Karyl <karylkenth@gmail.com>
2020-09-04 08:57:52 +0200

    Mise en place de socketio et fusion des applications

--- express.js
+++ express.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const connection = require('./database');
 
+// const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+
+router.get("/", (req, res) => {
+  // serve the index.html file
+  res.sendFile(__dirname + '/index.html')
+});
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true
@@ -18,9 +26,9 @@ router.post("/signup", (req, res) => {
     const { login, password, mail } = req.body;
     console.log(login, password, mail);
     console.log("called");
-    connection.query(`INSERT INTO user (login, password, mail, statut) VALUES ("${login}","${password}","${mail}", "1")`, (err, resultInsert) => {
+    connection.query(`INSERT INTO user (login, password, mail, status) VALUES ("${login}","${password}","${mail}", "1")`, (err, resultInsert) => {
       if(!err){
-        connection.query(`SELECT login, password, id_user, mail, statut FROM user WHERE login = "${login}" AND password = "${password}"`, (error, result) => {
+        connection.query(`SELECT login, password, id_user, mail, status FROM user WHERE login = "${login}" AND password = "${password}"`, (error, result) => {
           if(!error) {
             res.json(JSON.parse(JSON.stringify(result[0])));
           }
--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const backend = require('./express');
-const http = require("http");
-const io = require('socket.io')(http);
-var cors = require('cors')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server);
+const socket= require('socket.io');
+const cors = require('cors')
+
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/', backend);
+app.use(express.static('/bower_components'));
 
 // lancement du serveur port 8080
 app.use(function(req, res, next) {
@@ -19,13 +22,22 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-let server = app.listen( process.env.PORT || 3000, function() {
+server.listen( process.env.PORT || 3000, function() {
   console.log('Listening on port', server.address().port);
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  console.log('a user connected')
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  })
+});
+
+
+module.exports = app; 
+//   httpServer.listen(3000, () => {
+//   console.log('go to http://localhost:3000');
+// });
